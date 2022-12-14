798da052eb9db4ce68988056c70643e3ef1494ad
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-14 14:30:15 +0100

    milestone 1 inserimento contenuti dai data di vue e sottolineatura task done

--- js/script.js
+++ js/script.js
@@ -6,7 +6,44 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            tasks:[
+                {
+                    text: "Quarterly Newsletter",
+                    done: true
+                },
+                {
+                    text: "Rectruiting blog post",
+                    done: true
+                },
+                {
+                    text: "Mobile app launch",
+                    done: true
+                },
+                {
+                    text: "Interview with John H.",
+                    done: false
+                },
+                {
+                    text: "Summit update to mobile storefronts",
+                    done: true
+                },
+                {
+                    text: "Schedule meeting with Alex",
+                    done: false
+                },
+                {
+                    text: "Homepage refresh",
+                    done: true
+                },
+                {
+                    text: "Onboard new Sales team members",
+                    done: true
+                },
+                {
+                    text: "Review editorial calendar",
+                    done: true
+                }
+            ]
         }
     },
     methods: {
